71f67c27fd663a970b758bbeafac3ee5c3f27cdf
Momin Khan <foolmoron@gmail.com>
2016-03-29 01:51:45 -0400

    Improved string matcher

--- closestPlayer.js
+++ closestPlayer.js
@@ -1,7 +1,7 @@
 const _ = require('lodash');
-const levenshtein = (function() { // wrap levenshtein function to get 0.0 - 1.0 similarity range
+const levenshtein = (() => { // wrap levenshtein function to get 0.0 - 1.0 similarity range
     const fl = require('fast-levenshtein'); 
-    return function(a, b) { 
+    return (a, b) => { 
         return 1 - fl.get(a, b)/Math.max(a.length, b.length); 
     }; 
 })();
@@ -16,14 +16,21 @@ module.exports = (str, players) => {
         // indirect mention, need to use string distance algorithm to find player
         const levLimit = 0.5;
         const jwLimit = 0.7;
-        var playerNameComparisons = _.sortBy(_.map(players, function(player) {
-            return {
+        var playerNameComparisons = _.sortBy(_.flatten(_.map(players, player => {
+            var names = [player.name]; // full name is always a possible name
+            // also try names split up by capitalization or spaces
+            var splitNames = player.name.split(/([A-Z _][^A-Z _]*)/).filter(str => str.trim().length > 1);
+            if (splitNames.length > 1) {
+                names = names.concat(splitNames);
+            }
+            return names.map(name => ({
                 id: player.id,
                 name: player.name,
-                lev: levenshtein(str, player.name.toLowerCase()),
-                jw: jarowinkler(str, player.name.toLowerCase()),
-            }
-        }), function(item) {
+                distanceName: name.trim().toLowerCase(),
+                lev: levenshtein(str, name.trim().toLowerCase()),
+                jw: jarowinkler(str, name.trim().toLowerCase()),
+            }));
+        })), item => {
             var score = 0;
             if (item.lev >= levLimit) score += 1000;
             if (item.jw >= jwLimit) score += 1000;
