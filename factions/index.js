71a0197a0531f26d99221f87dfb21ddc237afd73
Momin Khan <foolmoron@gmail.com>
2016-04-02 18:03:10 -0400

    Role mixins fix

--- factions/index.js
+++ factions/index.js
@@ -1,7 +1,9 @@
+const ext = require('../lib/ext.js');
+
 var factions = [];
 require('fs').readdirSync(__dirname + '/').forEach(file => {
   if (file.match(/\.js$/) !== null && file !== 'index.js') {
-    var faction = require('./' + file);
+    var faction = ext({}, require('./' + file)); // clone object first so we don't pollute the require cache
     for (var prop in faction) {
         if (typeof(faction[prop]) === 'function') {
             faction[prop] = faction[prop].bind(faction);
