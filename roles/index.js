a2cf476eddce09c34d0108fc74726e9ee7ca7222
Momin Khan <foolmoron@gmail.com>
2016-04-02 18:13:20 -0400

    Fixed role.js

--- roles/index.js
+++ roles/index.js
@@ -2,7 +2,7 @@ const ext = require('../lib/ext.js');
 
 var roles = [];
 require('fs').readdirSync(__dirname + '/').forEach(file => {
-  if (file.match(/\.js$/) !== null && file.indexOf('cop') >= 0 && file !== 'index.js') {
+  if (file.match(/\.js$/) !== null && file !== 'index.js') {
     var role = ext({}, require('./' + file)); // clone object first so we don't pollute the require cache
     for (var prop in role) {
         if (typeof(role[prop]) === 'function') {
