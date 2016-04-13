124c20f5863902a9c1a0d0a9f3d4f16b6d2b732a
Momin Khan <foolmoron@gmail.com>
2016-04-12 20:46:12 -0400

    Fixed broken mafia-release

--- mafia-release.js
+++ mafia-release.js
@@ -3,7 +3,7 @@ var d = require('domain').create()
 var mafiabot = null;
 d.on('error', function(e){
     console.log(e.stack);
-    mafiabot.sendMessage(mafiabot.latestChannel, `💢 ***MafiaBot has encountered an error!***\n\`${e.stack.split('\n')[0]} - ${/\((.*\\)*(.*)\)/.exec(e.stack.split('\n')[1])[2]}\`\n\n**Restarting MafiaBot now... hopefully it won't happen again!**`).then(function() {
+    mafiabot.sendMessage(mafiabot.latestChannel, `💢 ***MafiaBot has encountered an error!***\n\`${e.stack.split('\n')[0]} - ${/\((.*\\)*(.*)\)/.exec(e.stack.split('\n')[1])[2]}\`\n\n**Restarting MafiaBot now... hopefully it won't happen again!**`, function() {
         process.exit(1);
     });
     setTimeout(function() { process.exit(1); }, 3000); // just in case
